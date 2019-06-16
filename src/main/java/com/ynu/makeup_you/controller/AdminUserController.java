0b5c5921c00fd1ba08b4ed11c0da4b34043bde40
Jianlong Hu <hujianlongynu@163.com>
2019-06-16 19:54:02 +0800

    2019-6-16 管理员登录

--- src/main/java/com/ynu/makeup_you/controller/AdminUserController.java
+++ src/main/java/com/ynu/makeup_you/controller/AdminUserController.java
@@ -3,6 +3,7 @@ package com.ynu.makeup_you.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.ynu.makeup_you.entity.AdminUser;
 import com.ynu.makeup_you.service.AdminUserService;
+import com.ynu.makeup_you.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +17,31 @@ import org.springframework.web.bind.annotation.*;
 public class AdminUserController {
     @Autowired
     AdminUserService adminUserService;
+    @Autowired
+    TokenService tokenService;
 
     JSONObject jsonObject;
+
+    // 用户登录
+    @PostMapping("/login")
+    public Object login(AdminUser au){
+        jsonObject = new JSONObject();
+        AdminUser auBase = adminUserService.findAdminByName(au.getName());
+        if (auBase == null){
+            jsonObject.put("message","登录失败,管理员不存在!");
+            return jsonObject;
+        }else{
+            if (!auBase.getPassword().equals(au.getPassword())){
+                jsonObject.put("message","登录失败,密码错误!");
+                return jsonObject;
+            }else{
+                String token = tokenService.getToken(au);
+                jsonObject.put("token",token);
+                jsonObject.put("AdminUser",auBase);
+                return jsonObject;
+            }
+        }
+    }
 
     // 增加管理员
     @PostMapping("/addAdminUser")
